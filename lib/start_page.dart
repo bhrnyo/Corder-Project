8ac8e0ee2c2fc40d7bf8eca97615d8022ac901d6
parkhayoung615 <parkhayoung615@gmail.com>
2023-06-15 16:40:11 +0900

    Update Project

--- lib/start_page.dart
+++ lib/start_page.dart
@@ -44,7 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
       backgroundColor: Colors.grey,
       body: Stack(children: [
         Swiper(
-          pagination: SwiperPagination(),
+          pagination: SwiperPagination(alignment: Alignment.bottomCenter),
           itemCount: imageList.length,
           itemBuilder: (context, index) {
             return Container(
@@ -52,27 +52,75 @@ class _MyHomePageState extends State<MyHomePage> {
                   image: DecorationImage(
                 image: AssetImage(imageList[index]),
                 fit: BoxFit.cover,
+                    colorFilter: ColorFilter.matrix(<double>[
+                      // 밝기를 조절하는 행렬 값 설정
+                      0.3, 0, 0, 0, 0,
+                      0, 0.3, 0, 0, 0,
+                      0, 0, 0.3, 0, 0,
+                      0, 0, 0, 1, 0,
+                    ]),
               )),
             );
           },
         ),
-        Center(
+        Stack(children: [
+          Positioned.fill(
+            bottom: -50,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-            Text(
-              '오직 개발자를 위한,',
-              style: TextStyle(
-                  fontSize: 30, color: Colors.white, fontFamily: 'Black'),
-            ),
-                Text(
-                  '오직 CORDER를 위한.',
-                  style: TextStyle(
-                      fontSize: 30, color: Colors.white, fontFamily: 'Black'),
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    text: '오직 개발자를 위한,\n',
+                    style: TextStyle(
+                        fontSize: 35,
+                        color: Colors.white,
+                        fontFamily: 'Black',
+                        height: 1),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: '오직 CORDER를 위한.',
+                        style: TextStyle(
+                            fontSize: 35,
+                            color: Colors.white,
+                            fontFamily: 'Black'),
+                      ),
+                      TextSpan(
+                        text: '\n\n당신에게 선사하는 ‘편한’ 코딩의 세계\n그 여정을 떠나기 위한 준비',
+                        style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.white,
+                            fontFamily: 'Thin'),
+                      )
+                    ],
+                  ),
                 ),
-          ],
-        ))
+                SizedBox(
+                  height: 70,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+
+                  },
+                  child: Text(
+                    '지금 바로 확인',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontFamily: 'Regular', fontSize: 17),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromRGBO(40, 74, 222, 1),
+                      minimumSize: Size(152, 54),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0), // 원하는 radius 값 설정
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ])
       ]),
     );
   }
