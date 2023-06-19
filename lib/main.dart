1d1f848cd13294bc55cb3e4cc49e420867280f62
parkhayoung615 <parkhayoung615@gmail.com>
2023-06-19 15:30:59 +0900

    Update Project

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,19 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Center(
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.supervisor_account), // 앞에 아이콘이 생성됨
+                border: OutlineInputBorder(),
+                labelText: '아이디를 입력하세요',
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
