9fb746d005adbe10eda5c7ca690e776a0da4fcc0
parkhayoung615 <parkhayoung615@gmail.com>
2023-06-19 16:46:57 +0900

    Update Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
+
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -9,19 +9,62 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  TextEditingController _textEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            TextField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.supervisor_account), // 앞에 아이콘이 생성됨
-                border: OutlineInputBorder(),
-                labelText: '아이디를 입력하세요',
+            Center(
+              child: Container(
+                width: 340,
+                child: TextField(
+                  controller: _textEditingController,
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    labelText: '검색어를 입력해 주세요',
+                  ),
+                ),
               ),
             ),
+            SizedBox(
+              height: 40,
+            ),
+            Text('인기 콘텐츠', style: TextStyle(fontSize: 24, fontFamily: 'Black'), textAlign: TextAlign.left,),
+            // Column(
+            //   children: [
+            //     ListView(
+            //       scrollDirection: Axis.vertical,
+            //       children: [
+            //         ListTile(
+            //           leading: Icon(Icons.home),
+            //           title: Text('Home'),
+            //           trailing: Icon(Icons.navigate_next),
+            //           onTap: () {},
+            //         ),
+            //         ListTile(
+            //           leading: Icon(Icons.event),
+            //           title: Text('Event'),
+            //           trailing: Icon(Icons.navigate_next),
+            //           onTap: () {},
+            //         ),
+            //         ListTile(
+            //           leading: Icon(Icons.camera),
+            //           title: Text('Camera'),
+            //           trailing: Icon(Icons.navigate_next),
+            //           onTap: () {},
+            //
+            //         )
+            //       ],
+            //     ),
+            //   ],
+            // ),
           ],
         ),
       ),
