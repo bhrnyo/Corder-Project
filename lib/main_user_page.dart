3dd206e41d56ca1d661ce50b09883a834fcdf325
parkhayoung615 <parkhayoung615@gmail.com>
2023-06-20 16:49:57 +0900

    Update main_user_page.dart

--- lib/main_user_page.dart
+++ lib/main_user_page.dart
@@ -12,7 +12,30 @@ class _MainUserPageState extends State<MainUserPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Center(
+        child: Card(
+          child: Column(
+            children: [
+              CircleAvatar(
+                child: Image.asset('assets/m2.PNG'),
+              ),
+              SizedBox(height: 20,),
+              Text('Java, Html'),
+              Text('name'),
+              SizedBox(height: 10,),
+              ElevatedButton(onPressed: () {}, child: Text('프로필 수정하기')),
+              SizedBox(height: 20,),
+              Row(
+                children: [
+                  Text('팔로잉\n9'),
+                  SizedBox(width: 40,),
+                  Text('팔로워\n1.3k'),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
