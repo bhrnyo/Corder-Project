8c64e23d71a5907c1583358b8e6e7499daeeba12
parkhayoung615 <parkhayoung615@gmail.com>
2023-06-20 16:51:42 +0900

    Update playlist_page.dart

--- lib/playlist_page.dart
+++ lib/playlist_page.dart
@@ -12,7 +12,34 @@ class _PlayListPageState extends State<PlayListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: Container(
+                width: 340,
+                child: TextField(
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    labelText: '검색어를 입력해 주세요',
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
 
+          ],
+        ),
+      ),
     );
   }
 }
